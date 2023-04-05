b9f712c8f51976e7ad57c8e554312c98089a1595
Aryan Kamboj <kambojaryankamboj@gmail.com>
2023-04-05 20:18:55 +0530

    Added unpickle file

---
+++ unpickle.py
@@ -0,0 +1,9 @@
+import pickle,stu
+
+with open('pickle2.dat','rb') as unpickle_file:
+    while True:
+        try:
+            obj=pickle.load(unpickle_file)
+            obj.display_student()
+        except EOFError:
+            print("Done!")
